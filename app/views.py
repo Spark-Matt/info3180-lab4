074541277578879a6534af16100bc23a19b6b29c
Matthew Patterson <matthew.patterson@mymona.uwi.edu>
2018-02-23 23:58:25 +0000

    Exercise 1- File Uploads

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from form import UploadForm
 
 
 ###
@@ -32,15 +33,26 @@ def upload():
         abort(401)
 
     # Instantiate your form class
+    photoform = UploadForm()
 
     # Validate file upload on submit
-    if request.method == 'POST':
+    if request.method == 'POST' and photoform.validate_on_submit():
         # Get file data and save to your uploads folder
+        
+        photo = photoform.photo.data
+        description = photoform.description.data
 
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(
+            app.config['UPLOAD_FOLDER'], filename
+        ))
+        
+        
         flash('File Saved', 'success')
         return redirect(url_for('home'))
-
-    return render_template('upload.html')
+        
+    flash_errors(photoform)
+    return render_template('upload.html', form=photoform)
 
 
 @app.route('/login', methods=['POST', 'GET'])
